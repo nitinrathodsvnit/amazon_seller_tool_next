3865ac7436b1c2862d518d8b7ba9622044212a49
nitinrathodsvnit <nitinrathod2300@gmail.com>
2024-08-28 13:08:16 +0530

    add user authentication
    Co-authored-by: Aniket Rana <Aniumbott@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,12 @@
 "use client";
-import { useState, useEffect } from "react";
-import Result from "@/components/Result";
-import Loader from "@/components/Loader";
-import Chart from "@/components/Chart";
-
-import Calculator from "@/components/Calculator";
-import Records from "@/components/Records";
 import Navbar from "@/components/Navbar";
 import Landing from "@/components/Landing";
 
 export default function Home() {
-  const [referralFees, setReferralFees] = useState([]);
-  const [shippingFees, setShippingFees] = useState([]);
-  const [closingFees, setClosingFees] = useState([]);
-
-  const [frm, setFrm] = useState({
-    categoryId: "",
-    price: 0,
-    shippingType: "",
-    shippingFeesObj: "",
-    weight: 200,
-    shippingRegion: "",
-    weightType: "",
-  });
-
-  useEffect(() => {
-    const getQuery = async (query: string) => {
-      const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + query);
-      const data = await res.json();
-      return data;
-    };
-    getQuery("referralfees").then((data) => setReferralFees(data));
-    getQuery("closingfees").then((data) => setClosingFees(data));
-    getQuery("shippingfees").then((data) => setShippingFees(data));
-  }, []);
-  let isReady: boolean =
-    referralFees.length > 0 &&
-    closingFees.length > 0 &&
-    shippingFees.length > 0;
-  return !isReady ? (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <Loader />
-    </div>
-  ) : (
+  return(
     <main className="flex flex-col min-h-screen gap-4 px-4">
       <Navbar />
       <Landing />
-      {/* <div className="flex flex-col gap-4">
-        <div className="flex gap-4 w-full">
-          <Calculator
-            frm={frm}
-            setFrm={setFrm}
-            referralFees={referralFees}
-            closingFees={closingFees}
-            shippingFees={shippingFees}
-          />
-          <Chart
-            referralFees={referralFees}
-            closingFees={closingFees}
-            shippingFees={shippingFees}
-            frm={frm}
-          />
-        </div>
-        <Records />
-      </div> */}
     </main>
-  );
+  )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { SignUpButton, UserButton } from "@clerk/nextjs";
+import { useUser } from "@clerk/clerk-react";
 
 const Navbar: React.FC = () => {
+  const { isSignedIn } = useUser();
   return (
     <nav className="flex items-center justify-between p-2 bg-black text-white rounded-xl my-4 fixed top-0 w-[calc(100%-32px)] z-[100]">
       <div className="flex items-center p-2">
-        <Link href="/" className="text-xl text-wrap font-bold flex items-center">
+        <Link
+          href="/"
+          className="text-xl text-wrap font-bold flex items-center"
+        >
           SellerROI
         </Link>
       </div>
@@ -18,20 +24,42 @@ const Navbar: React.FC = () => {
         <NavLink href="/settings">Settings</NavLink>
       </div>
       <div className="flex space-x-2">
-      <Button variant="outline" className="bg-white text-black rounded-lg p-5">
-        Sign In
-      </Button>
-      <Button variant="outline" className="bg-white text-black rounded-lg p-5">
-        Sign Up
-      </Button>
+        {isSignedIn ? (
+          <div className="pr-2 flex justify-center items-center">
+          <UserButton appearance={
+          {
+            elements: {
+              userButtonAvatarBox: {
+                height: 40,
+                width: 40,
+              }
+          },
+          }} />
+          </div>
+        ) : (
+          <SignUpButton mode="modal">
+            <Button
+              variant="outline"
+              className="bg-white text-black rounded-lg p-5"
+            >
+              Sign In / Up
+            </Button>
+          </SignUpButton>
+        )}
       </div>
     </nav>
   );
 };
 
-const NavLink: React.FC<{ href: string; children: React.ReactNode }> = ({ href, children }) => {
+const NavLink: React.FC<{ href: string; children: React.ReactNode }> = ({
+  href,
+  children,
+}) => {
   return (
-    <Link href={href} className="text-gray-400 hover:text-white transition-colors">
+    <Link
+      href={href}
+      className="text-gray-400 hover:text-white transition-colors"
+    >
       {children}
     </Link>
   );
